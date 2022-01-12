078f38cb4acd94e3c86c5efa16aab451c093cd10
wortin <wortin@outlook.com>
2022-01-12 21:53:31 +0800

    feat: thread state

---
+++ src/test/java/com/wortin/piqln/thread/ThreadTest.java
@@ -0,0 +1,14 @@
+package com.wortin.piqln.thread;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ThreadTest {
+
+    @Test
+    public void TestThreadState() {
+        Assertions.assertEquals(1, 1);
+    }
+
+
+}
